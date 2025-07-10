35f978825f487b9a83eefa940a9b23a03c39acd8
srihari-aaha <srihariaahasolution@gmail.com>
2025-07-10 10:29:54 +0530

    bottom rearanged

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -205,8 +205,8 @@ const BottomTabNavigator = () => {
 			}}
 		>
 			<Tab.Screen name="Home" component={VideoListScreen} options={{ title: 'Home' }} />
-			<Tab.Screen name="Payment" component={PaymentScreen} options={{ title: 'Payment' }} />
 			<Tab.Screen name="level" component={VideoListScreen} options={{ title: 'Age' }} />
+			<Tab.Screen name="Payment" component={PaymentScreen} options={{ title: 'Payment' }} />
 			<Tab.Screen name="Setting" component={SettingScreen} options={{ title: 'Setting' }} />
 		</Tab.Navigator>
 	);
